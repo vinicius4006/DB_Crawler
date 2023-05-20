74c65020f7f6bf2ca3978988f261ed20ff069d47
Elvis-Almeida <lindete.r.almeida@gmail.com>
2023-05-20 13:19:51 -0300

    Atualização códigos python

--- python-script/Web_crawler.py
+++ python-script/Web_crawler.py
@@ -1,8 +1,17 @@
 import threading
 import requests
 from bs4 import BeautifulSoup
-from create_terms import criar_vetor_de_termos
-from api_data_base import post_request_with_base64, post_words, post_meta_tags
+from api_data_base import *
+
+def extract_body_content(soup):
+    body = soup.find('body')
+    if body:
+        content = body.get_text()
+        content = '\n'.join(line for line in content.splitlines() if line.strip())
+        return content
+    else:
+        return ''
+ 
 
 def get_meta_tags(soup):
     meta_tags = soup.find_all('meta')
@@ -13,11 +22,10 @@ def get_meta_tags(soup):
     
     return meta_tag_list
 
-def get_body_content(soup):
-    return soup.body
 
 def remove_repeated_items(list_links):
     return list(set(list_links))
+
 
 def filter_https_links(list_links):
     filtered_links = []
@@ -26,55 +34,75 @@ def filter_https_links(list_links):
             filtered_links.append(link)
     return filtered_links
 
-def get_links(url, profundidade, contador):
+def get_links(soup):
+    list_links = []
+    for tag_a in soup.find_all('a'):
+        link = tag_a.get('href')
+        if link:
+            list_links.append(link)
+    return list_links
+
+
+def web_crawler(url, profundidade, contador):
+    print('\n', contador, url)
+
     try:
         response = requests.get(url, timeout=100).content
         soup = BeautifulSoup(response, 'html.parser')
-        vetor_de_termos = criar_vetor_de_termos(soup)
-        print('\n', contador, url)
-        
-        listaTemp = dict()
-        listaTemp['url'] = url
-        list_meta_tag = get_meta_tags(soup)
-        body = get_body_content(soup)
-        
-        siteid = post_request_with_base64(url, body)
-        post_meta_tags(list_meta_tag, siteid)
-        post_words(siteid, vetor_de_termos)
-       
-        if profundidade == contador:
-            print('chegou na profundidade\n')
-            return
+
+    except Exception as e:
+        print("Erro ao acessar:", e, url)
+        return
     
-        contador += 1
+    try:
+        list_meta_tag = get_meta_tags(soup)
+        body = extract_body_content(soup)
         
-        list_links = []
-        for tag_a in soup.find_all('a'):
-            link = tag_a.get('href')
-            if link:
-                list_links.append(link)
+        siteid = post_body(url, body)
+        post_meta_tags(list_meta_tag, siteid)
+    except Exception as e:
+        print("Erro no Banco de dados:", e, url)
+        return
         
+    if profundidade == contador:
+        print('Chegou na profundidade')
+        return
+
+    contador += 1
+    
+
+    try:
+        list_links = get_links(soup)
         list_links = remove_repeated_items(list_links)
         list_links = filter_https_links(list_links)
-        
-        threads = []
-        for link in list_links:
-            t = threading.Thread(target=get_links, args=(link, profundidade, contador))
-            threads.append(t)
-            t.start()
-        
-        for t in threads:
-            t.join()
 
-    except requests.exceptions.Timeout:
-        print("Timeout de conexão. ", url)
-        return
-    
     except Exception as e:
-        print("Ocorreu um erro:", e, url)
+        print("Erro em capturar links", e, url)
         return
 
+    threads = []
+    for link in list_links:
+        t = threading.Thread(target=web_crawler, args=(link, profundidade, contador))
+        threads.append(t)
+        t.start()
+    
+    for t in threads:
+        t.join()
 
-get_links('https://recordtv.r7.com/', 1, 0)
+    return 'Terminou'
+   
+
+
+a = web_crawler('https://www.folha.uol.com.br', 1, 0)
+print(a)
+# web_crawler('https://oglobo.globo.com/', 1, 0)
+# web_crawler('https://www.folha.uol.com.br/', 1, 0)
+# web_crawler('https://www.estadao.com.br/', 1, 0)
+# web_crawler('https://valor.globo.com/', 1, 0)
+# web_crawler('https://ahnegao.com.br/', 2, 0)
+# web_crawler('https://gauchazh.clicrbs.com.br/', 1, 0)
+# web_crawler('https://www.jb.com.br/', 1, 0)
+# web_crawler('https://www.nsctotal.com.br/diarinho', 2, 0)
+# web_crawler('https://www.opovo.com.br/', 1, 0)
 
 
