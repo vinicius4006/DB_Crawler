c8b74bb72ab66731c85e81574cadb87d67962fc3
Elvis-Almeida <lindete.r.almeida@gmail.com>
2023-05-20 18:49:04 -0300

    Adicionando front

--- Front/script_index.js
+++ Front/script_index.js
@@ -1,22 +1,18 @@
 function seach(input) {
     let texto = input.value.trim();
+    let link_enfeite = 'http://127.0.0.1:5500/search?q=' + encodeURIComponent(texto).replace(/%20/g, '+')
     let link = 'http://127.0.0.1:5500/search?q=' + encodeURIComponent(texto).replace(/%20/g, '+')
     
-    console.log(link);
-
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(data => {
-        data['linkpesquisa'] = link
+        data['linkpesquisa'] = link_enfeite
         let jsonData = JSON.stringify(data);
-        // Codifica a string em base64
         let base64Data = btoa(jsonData);
-        // Abre a página Y com o JSON como parâmetro na URL
         window.location.href = "result.html?q=" + encodeURIComponent(base64Data);
     })
     .catch(error => {
-      // Em caso de erro na requisição
-      console.log(error);
+        console.log(error);
     });    
 }
 
--- Front/script_result.js
+++ Front/script_result.js
@@ -1,6 +1,21 @@
 function show_results(data) {
     history.pushState(null, null, data['linkpesquisa']);
     console.log(data);
+
+    let container = document.getElementById('container')
+    for (let index = 0; index < 10; index++) {
+        container.innerHTML += 
+        '<div id="container">' +
+            '<div class="container_site">' +
+                '<a href="#">' +
+                    '<p class="url">google.com</p>' +
+                    '<p class="full_url">google.com/jogos/online/issomermo</p>' +
+                    '<h2>Como acessar jogos online</h2>' +
+                '</a>' +
+                '<p class="description">Todos os arquivos de Assistência Técnica - Documentação básica 1. Assistência Técnica - Documentação básica 10. Assistência Técnica - Documentação básica 10 ...</p>' +
+            '</div>' +
+        '</div>'
+    }
 }
 
 var urlParams = new URLSearchParams(window.location.search);
