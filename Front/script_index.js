288b7fb13e393c36bb7001924a2e8ca4e3698a93
Elvis-Almeida <lindete.r.almeida@gmail.com>
2023-05-20 21:22:54 -0300

    Adicionando o front

--- Front/script_index.js
+++ Front/script_index.js
@@ -1,3 +1,125 @@
+let resposta = [
+	{
+		"ID": 219536,
+		"CreatedAt": "2023-05-19T17:16:04.312771-03:00",
+		"UpdatedAt": "2023-05-19T17:16:04.312771-03:00",
+		"DeletedAt": null,
+		"SiteID": 1479,
+		"Ref": {
+			"ID": 1479,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://adorofutebol.com.br",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	},
+	{
+		"ID": 268874,
+		"CreatedAt": "2023-05-19T17:25:05.08377-03:00",
+		"UpdatedAt": "2023-05-19T17:25:05.08377-03:00",
+		"DeletedAt": null,
+		"SiteID": 1669,
+		"Ref": {
+			"ID": 1669,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://gda.com/",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 3
+	},
+	{
+		"ID": 358579,
+		"CreatedAt": "2023-05-19T17:37:56.10388-03:00",
+		"UpdatedAt": "2023-05-19T17:37:56.10388-03:00",
+		"DeletedAt": null,
+		"SiteID": 2156,
+		"Ref": {
+			"ID": 2156,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://adorofutebol.com.br",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	},
+	{
+		"ID": 372241,
+		"CreatedAt": "2023-05-19T18:04:39.281663-03:00",
+		"UpdatedAt": "2023-05-19T18:04:39.281663-03:00",
+		"DeletedAt": null,
+		"SiteID": 2173,
+		"Ref": {
+			"ID": 2173,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://www.mongehumorista.com/",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	},
+	{
+		"ID": 377620,
+		"CreatedAt": "2023-05-19T18:05:19.713143-03:00",
+		"UpdatedAt": "2023-05-19T18:05:19.713143-03:00",
+		"DeletedAt": null,
+		"SiteID": 2190,
+		"Ref": {
+			"ID": 2190,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://www.mensagensdebomdia.com.br/",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	},
+	{
+		"ID": 378785,
+		"CreatedAt": "2023-05-19T18:08:55.10239-03:00",
+		"UpdatedAt": "2023-05-19T18:08:55.10239-03:00",
+		"DeletedAt": null,
+		"SiteID": 2217,
+		"Ref": {
+			"ID": 2217,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://www.mongehumorista.com/",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	},
+	{
+		"ID": 387220,
+		"CreatedAt": "2023-05-19T18:09:25.885555-03:00",
+		"UpdatedAt": "2023-05-19T18:09:25.885555-03:00",
+		"DeletedAt": null,
+		"SiteID": 2199,
+		"Ref": {
+			"ID": 2199,
+			"CreatedAt": "0001-01-01T00:00:00Z",
+			"UpdatedAt": "0001-01-01T00:00:00Z",
+			"DeletedAt": null,
+			"Url": "https://www.mensagensdebomdia.com.br/",
+			"Body": ""
+		},
+		"Value": "inicio",
+		"Counter": 1
+	}
+]
+
 function seach(input) {
     let texto = input.value.trim();
     let link_enfeite = 'http://127.0.0.1:5500/search?q=' + encodeURIComponent(texto).replace(/%20/g, '+')
@@ -6,13 +128,14 @@ function seach(input) {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(data => {
-        data['linkpesquisa'] = link_enfeite
-        let jsonData = JSON.stringify(data);
-        let base64Data = btoa(jsonData);
-        window.location.href = "result.html?q=" + encodeURIComponent(base64Data);
+        resposta['linkpesquisa'] = link_enfeite
+        console.log(resposta);
+        let jsonData = JSON.stringify(resposta)
+        let base64Data = btoa(jsonData)
+        window.location.href = "result.html?q=" + encodeURIComponent(base64Data)
     })
     .catch(error => {
-        console.log(error);
+        console.log(error)
     });    
 }
 
@@ -26,3 +149,4 @@ input.addEventListener('keydown', function(event) {
 document.getElementById("enviarJson").addEventListener("click", function() {
     seach(input)
 });
+
--- Front/script_result.js
+++ Front/script_result.js
@@ -3,19 +3,40 @@ function show_results(data) {
     console.log(data);
 
     let container = document.getElementById('container')
-    for (let index = 0; index < 10; index++) {
+    for (let i = 0; i < data.length; i++) {
+        linkcompleto = data[i].Ref.Url
+        let linkcurto = extractDomain(linkcompleto)
         container.innerHTML += 
         '<div id="container">' +
             '<div class="container_site">' +
-                '<a href="#">' +
-                    '<p class="url">google.com</p>' +
-                    '<p class="full_url">google.com/jogos/online/issomermo</p>' +
+                '<a href="'+ linkcompleto +'">' +
+                    '<p class="url">'+ linkcurto +'</p>' +
+                    '<p class="full_url">'+ linkcompleto +'</p>' +
                     '<h2>Como acessar jogos online</h2>' +
                 '</a>' +
                 '<p class="description">Todos os arquivos de Assistência Técnica - Documentação básica 1. Assistência Técnica - Documentação básica 10. Assistência Técnica - Documentação básica 10 ...</p>' +
             '</div>' +
         '</div>'
     }
+    container.innerHTML += '<div id="space"></div>'
+    revovePreload()
+}
+
+function extractDomain(url) {
+    // Remove o protocolo (http://, https://) da URL
+    var domain = url.replace(/(^\w+:|^)\/\//, '');
+  
+    // Remove o caminho após a primeira barra
+    domain = domain.split('/')[0];
+  
+    // Remove www. se estiver presente
+    domain = domain.replace(/^www\./, '');
+  
+    return domain;
+}
+
+function revovePreload() {
+    // document.getElementById('preload').style.transform = 'translate(0%, -100%)'
 }
 
 var urlParams = new URLSearchParams(window.location.search);
